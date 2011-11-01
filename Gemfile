d7eec4a17aec6711b943ebf80797e4cf7e71406b
Matt Beedle <mattbeedle@googlemail.com>
2011-11-01 11:42:52 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'bson_ext'
+gem 'jquery-rails'
+gem 'mongoid'
+gem 'rails', '~> 3.1.1'
+gem 'slim-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'fabrication'
+  gem 'cucumber-rails'
+end
+
+group :development do
+  gem 'heroku'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LandingPage::Application.config.secret_token = 'a09590692d9303b96c14057419b014f9b8b2a37c53f8ec8864ca6b43806af27737cf04aa9c99030a4a87ea9e1e1392cb6b2d73da8a5a3be278667e4d2b9c7f72'
